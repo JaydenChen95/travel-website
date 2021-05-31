3a563f313611035de471cbbfbe3bb214dc365050
JaydenChen95 <66548474+JaydenChen95@users.noreply.github.com>
2021-05-31 23:18:20 +0800

    Updated secret-area.js for CORS issue

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,4 +1,17 @@
 exports.handler = (event, context, callback) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type"
+  }
+
+  if (event.httpMethod !== "POST") {
+    return callback(null, {
+      statusCode: 200,
+      headers,
+      body: "This was not a POST request"
+    })
+  }
+
   const secretContent = `
     <h3>Congratulations!</h3>
     <p>You cracked the pass code!<p>
